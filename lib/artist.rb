5c46511fd276d9bd67c8a4d1b5fa50794ba1c2f2
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-12 11:33:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,10 +15,9 @@ def add_song(song)
 song.artist = self
 end
 
-def add_song_by_name(song_name)
-song_name = Song.new(song_name)
-add_song(song_name)
-end
+def self.find_or_create_by_name(name)
+   self.all.detect {|artist| artist.name == name} || Artist.new(name).save
+ end
 
 def self.song_count
 Song.all.count
